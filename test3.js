8437b4308beb62dcf4216d08a1f7759821271168
natgonzalez2329 <natgonzalez2329@gmail.com>
2021-12-08 21:22:25 -0300

    feat: test3 db constructor

--- test3.js
+++ test3.js
@@ -1,11 +1,16 @@
 const sift = require('sift');
-const {} = require('./test/utils/errors');
+const { NotFound, ServerError } = require('./test/utils/errors');
 
 // This is a bonus but you need to do the previous tests before
 
 class Server {
     // Create your server with query features with sift
     // We pass the db when the Server is instantiated (constructor)
+    
+  constructor(db) {
+    this.db = db;
+  }
+
 }
 
 module.exports = Server;
